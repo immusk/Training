74c044c0176163253cb32801776ff79939c40e97
immusk <imranrehman5155@gmail.com>
2015-08-20 14:13:40 +0530

    this is my first commit

---
+++ WebdriverTest/src/test/java/testing/Jenkins.java
@@ -0,0 +1,40 @@
+package testing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Jenkins {
+
+	public WebDriver driver;
+	@Test
+	public void openGoogle() {
+
+		driver = new FirefoxDriver();
+		driver.get("https://www.google.co.in/");
+		Assert.assertEquals(driver.getTitle(), "Google", "Expected page is not opened");
+		driver.quit();
+	}
+	
+	@Test
+	public void doGoogleSearch() {
+
+		driver = new FirefoxDriver();
+		driver.get("https://www.google.co.in/");
+		driver.findElement(By.name("q")).sendKeys("Selenium");
+		Assert.assertEquals(driver.getTitle(), "Google", "Expected page is not opened");
+		driver.quit();
+	}
+	
+	@Test
+	public void doYahooSearch() {
+
+		driver = new FirefoxDriver();
+		driver.get("https://www.yahoo.com/");
+		driver.findElement(By.id("UHSearchBox")).sendKeys("Selenium");
+		Assert.assertEquals(driver.getTitle(), "Yahoo", "Expected page is not opened");
+		driver.quit();
+	}
+}
